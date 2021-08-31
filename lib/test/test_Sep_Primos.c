024f6bbbb7177ce48d148c2e2ff00e5f18087e2c
Luis-Herz <2153068030@cua.uam.mx>
2021-08-31 18:25:33 -0500

    Se corrigio la direccion de los archivos al directorio test

--- lib/test/test_Sep_Primos.c
+++ lib/test/test_Sep_Primos.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "./lib/src/Sep_Primos.c"
+#include "../src/Sep_Primos.c"
 
 int** create_square_matrix(size_t dim);
 void llenar (int **, size_t, int);
 void imprimir(int **M, size_t Dim);
 int pares(int **Ma, int Dim);
 void imprimir_M(int **M,int Dim);
-//aquí coloquen la cabecera de su función.
+
 void imprimir_A(int *Ar, int Dim);
 
 int main(int argc, char const *argv[]){
@@ -19,10 +19,8 @@ int main(int argc, char const *argv[]){
     llenar(Matriz, Dimension, coso_para_random);
 
     imprimir_M(Matriz, Dimension);
-
-    //int PARES = pares(Matriz, Dimension);     IGNORAR ESTÓ... POR AHORA
     
-    int* ArregloPares= Sep_Primos(Matriz, Dimension); //Aquí llamen a su funcion;
+    int* ArregloPares= Sep_Primos(Matriz, Dimension);
     
     int i=0;
     printf("Primos: \n");
@@ -31,7 +29,6 @@ int main(int argc, char const *argv[]){
         i++;
     } while (ArregloPares[i] != 0);
     
-    //imprimir_A(ArregloPares, 13);
     return 0;
 }
 
@@ -58,32 +55,4 @@ void imprimir_M(int **M,int Dim){
             }
         printf("\n");
         }
-}
-
-/* IGNORAR ESTO... POR AHORA
-int pares(int **Ma, int Dim){ 
-    int Par=0;
-    for(int i=0; i<Dim; i++){
-        for(int j=0; j<Dim; j++){
-            if ((Ma[i][j]%2) == 0 && (Ma[i][j]) != 0){
-                Par += 1;
-            }
-        }
-    }
-    printf("hay %i pares\n", Par);
-    return Par;
-}
-*/
-
-
-/*-----------------------------------------------------------
-Aquí implementen su función.
------------------------------------------------------------*/
-
-
-void imprimir_A(int *Ar, int Dim){
-    printf("El arreglo de numeros Primos es: \n");
-    for(int i=0; i<Dim; i++){
-        printf("[%i] ", Ar[i]);
-    }
 }
